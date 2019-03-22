f454f2b6a5a6645015b836a83033e91985ae65d6
roninhaf <roninhaf@gmail.com>
2019-03-22 18:58:17 -0400

    Cahnged inline style

--- MyApp/src/component/Joke.js
+++ MyApp/src/component/Joke.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Joke(props) {
     return (
-        <div>
+        <div className="todo-item">
             <p style={{display: !props.question && 'none'}}>Question: {props.question}</p>
             <p style={{backgroundColor: !props.question && 'gray'}}>Punchline: {props.punchline}</p>
         </div>
