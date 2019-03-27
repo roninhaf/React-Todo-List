d8718a0b992976a31c52143a6dc3666ad9d4a6a1
roninhaf <roninhaf@gmail.com>
2019-03-27 14:22:44 -0400

    Todo app done

--- MyApp/src/component/App.js
+++ MyApp/src/component/App.js
@@ -2,14 +2,38 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./TodosData";
 
-const data = todosData.map(element => <TodoItem key={element.id} text={element.text} element={element} />);
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: todosData
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-function App() {
-    return (
-        <div className="todo-list">
-            {data}
-        </div>
-    )
+    handleClick(id) {
+        this.setState(
+            prevState => {
+                const updatedTodo = prevState.todos.map(element => {
+                    if(element.id === id) {
+                        element.completed = !element.completed;
+                    }
+                    return element;
+                })
+                return {todos: updatedTodo};
+            }
+        )
+    }
+
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} handleChange={this.handleClick} item={item}/>);
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )
+
+    }
 }
 
 export default App
--- MyApp/src/component/TodoItem.js
+++ MyApp/src/component/TodoItem.js
@@ -2,10 +2,15 @@ import React from "react";
 
 
 function TodoItem(props) {
+    const doneStyle = {
+        fontStyle: "italic",
+        textDecoration: "line-through",
+        color: "#cdcdcd"
+    }
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.element.completed}/>
-            <p>{props.text}</p>
+            <input type="checkbox" checked={props.item.completed} onClick={() => {props.handleChange(props.item.id)}}/>
+            <p style={props.item.completed ? doneStyle : null}>{props.item.text}</p>
         </div>
     )
 }
